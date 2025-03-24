87cbffdbae72a2780b2503b56170977fa4d0b38c
Baajiinn <craftesdras@gmail.com>
2025-03-24 21:59:20 +0100

    feat : lightbox effet sur photo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,25 @@
-console.log("La hauteur totale de mon site web est de : " + document.documentElement.scrollHeight + " pixels");
+$(document).ready(function() {
+    console.log("La hauteur totale de mon site web est de : " + document.documentElement.scrollHeight + " pixels");
+
+    $('#photo').on('click', function(event) {
+        event.stopPropagation(); // Empêche la propagation du clic pour éviter un conflit avec le clic en dehors
+        $(this).toggleClass("zoomed");
+        $('.photoHolder').toggleClass("photoHolderZoomed");
+    });
+
+    // Dézoom avec la touche Échap
+    $(document).on('keydown', function(event) {
+        if (event.key === "Escape" && $('#photo').hasClass("zoomed")) {
+            $('#photo').removeClass("zoomed");
+            $('.photoHolder').removeClass("photoHolderZoomed");
+        }
+    });
+
+    // Dézoom en cliquant en dehors de l’image
+    $(document).on('click', function() {
+        if ($('#photo').hasClass("zoomed")) {
+            $('#photo').removeClass("zoomed");
+            $('.photoHolder').removeClass("photoHolderZoomed");
+        }
+    });
+});
